1e32e54c454809224a70cc8deac556ea15775bf6
proring12 <rlaflsk5142@naver.com>
2020-08-21 17:02:17 +0900

    JS: navbar menu click

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 'use strict';
 
+/* navbar sticky */
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll',() => {
-    console.log(window.scrollY);
-    console.log(`navbarHiehgt: ${navbarHeight}`);
+document.addEventListener("scroll",() => {
+    // console.log(window.scrollY);
+    // console.log(`navbarHiehgt: ${navbarHeight}`);
     if(window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
     }
+})
+
+/* navbar & contact button Handle Scrolling when tapping */
+const navbarMenu = document.querySelector(".navbar__menu")
+navbarMenu.addEventListener("click", () => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    // console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
 })
