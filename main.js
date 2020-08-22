4a0d27545f6d9a6f436c8d5420a69ed2f0717ba6
pororing12 <42339050+pororing12@users.noreply.github.com>
2020-08-22 15:22:33 +0900

    JS: toggle button click navbarMenu

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* navbar sticky */
+// navbar sticky
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll",() => {
@@ -13,13 +13,10 @@ document.addEventListener("scroll",() => {
     }
 })
 
-function ScrollIntoView(selector) {
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior: "smooth"});
-}
 
-/* navbar & contact button Handle Scrolling when tapping */
-const navbarMenu = document.querySelector(".navbar__menu")
+
+// navbar & contact button Handle Scrolling when tapping 
+const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", () => {
     const target = event.target;
     const link = target.dataset.link;
@@ -27,11 +24,45 @@ navbarMenu.addEventListener("click", () => {
         return;
     }
     // console.log(event.target.dataset.link);
+    navbarMenu.classList.remove("open");
     ScrollIntoView(link);
 })
 
+//hamburger bar click navbarMenu display
+const homeToggleBtn = document.querySelector(".navbar__toggle-btn");
+homeToggleBtn.addEventListener("click", () => {
+    navbarMenu.classList.toggle("open");
+})
+
+// contact button -> contact page
 const homeContact = document.querySelector(".home__contact");
 homeContact.addEventListener("click", () => {
     ScrollIntoView("#contact");
 })
 
+// Make home slowly fade
+const home = document.querySelector(".home__container");
+const homeHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+})
+
+//Arrow-up button Handle Scrolling when tapping 
+const arrowBtn = document.querySelector(".arrow-up");
+const arrowHeight = arrowBtn.getBoundingClientRect().height;
+arrowBtn.addEventListener("click", () => {
+    ScrollIntoView(".home");
+})
+document.addEventListener("scroll", () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowBtn.classList.add("visible");
+    } else {
+        arrowBtn.classList.remove("visible");
+    }
+})
+
+
+function ScrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
